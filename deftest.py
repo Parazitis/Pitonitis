00087b7ab923bcf50987b26e5897f1646d71eec3
Semirs Gerkans <kristians.svetins@bvsk.lv>
2022-10-19 23:24:30 +0300

    Updated deftest
    Testing string hello to eachother

--- deftest.py
+++ deftest.py
@@ -1,1 +1,13 @@
-print("Ludzu")
+x = str(input())
+if x == "lapas":
+    i = 0
+    while i<5:
+        print (x[i])
+        i = i+1
+else:
+    print(len(x))
+
+def greeting(x):
+    print("Greetings " + x)
+greeting(x)
+
