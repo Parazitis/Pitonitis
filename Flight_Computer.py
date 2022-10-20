ad2b441b497097c3588fd84a57473e1c68897f4c
parazits1 <kristians.svetins@bvsk.lv>
2022-10-20 22:33:00 +0300

    Flight Computer
    Pievienoju pirmo flight Computer versiju

---
+++ Flight_Computer.py
@@ -0,0 +1,33 @@
+initial= str(input("Enter Altitude or Temperature. To determine pressure under isa conditions(Max 10000 ft)  "))
+
+    
+def iqh_temp(temperature):
+    isa_dev = temperature - 15
+    altitude = isa_dev*-500
+    return altitude
+
+def iqh_alt(altitude):
+    indicated_pressure = 1013-altitude//30
+    print("The pressure is ", indicated_pressure,    "hPa at an altitude of ", altitude , "feet AMSL.(ISA)" )
+    return indicated_pressure
+
+if initial == "altitude":
+        altitude = int(input("Enter current altitude in feet(only numbers)   "))
+        if altitude>10000:
+            print("Altitude too high(Max 10000feet)")
+        else:
+            iqh_alt(altitude)
+elif initial == "temperature":
+        temperature = int(input("enter current temperature(In Celsius, only numbers)  " ))
+        iqh_temp(temperature)
+        altitude = iqh_temp(temperature)
+        iqh_alt(altitude)
+else:
+    print(initial, "is an invalid value, try again.")
+    
+
+
+
+
+
+
